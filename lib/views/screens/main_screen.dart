67c74008ea3e9a5738614ae5bd9b32a2c3300e94
Jean <acalto2@gmail.com>
2023-12-16 23:00:29 +0100

    30. SideBar Screens Final

--- lib/views/screens/main_screen.dart
+++ lib/views/screens/main_screen.dart
@@ -27,13 +27,19 @@ class _MainScreenState extends State<MainScreen> {
       sideBar: const SideBar(
         items: [
           AdminMenuItem(
-              title: 'Categories', icon: Icons.category_rounded, route: ''),
+              title: 'Dashboard', icon: Icons.dashboard_rounded, route: ''),
           AdminMenuItem(
-              title: 'Categories', icon: Icons.category_rounded, route: ''),
+              title: 'Vendors', icon: Icons.people_rounded, route: ''),
           AdminMenuItem(
-              title: 'Categories', icon: Icons.category_rounded, route: ''),
+              title: 'Withdrawal', icon: Icons.attach_money, route: ''),
           AdminMenuItem(
-              title: 'Categories', icon: Icons.category_rounded, route: ''),
+              title: 'Orders', icon: Icons.shopping_cart_rounded, route: ''),
+              AdminMenuItem(
+              title: 'Categories', icon: Icons.category, route: ''),
+              AdminMenuItem(
+              title: 'Products', icon: Icons.shopping_cart_rounded, route: ''),
+              AdminMenuItem(
+              title: 'Upload Banners', icon: Icons.add_a_photo_rounded, route: ''),
         ],
         selectedRoute: '',
       ),
