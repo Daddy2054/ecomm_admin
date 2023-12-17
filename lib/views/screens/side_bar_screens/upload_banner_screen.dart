c06207f84dd5b39e3495f7892fd0541537d2cbcc
Jean <acalto2@gmail.com>
2023-12-17 22:48:02 +0100

    34. Start Building

--- lib/views/screens/side_bar_screens/upload_banner_screen.dart
+++ lib/views/screens/side_bar_screens/upload_banner_screen.dart
@@ -13,13 +13,88 @@ class UploadBannerScreen extends StatelessWidget {
             alignment: Alignment.topLeft,
             padding: const EdgeInsets.all(10),
             child: const Text(
-              'Manage Upload Banners',
+              'Banners',
               style: TextStyle(
                 fontWeight: FontWeight.w700,
                 fontSize: 36,
               ),
             ),
           ),
+          const Divider(
+            color: Colors.grey,
+          ),
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(14.0),
+                child: Column(
+                  children: [
+                    Container(
+                      height: 140,
+                      width: 150,
+                      decoration: BoxDecoration(
+                        color: Colors.grey.shade500,
+                        border: Border.all(
+                          color: Colors.grey.shade800,
+                        ),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      // child: Center(
+                      //   child: _image != null
+                      //       ? Image.memory(_image)
+                      //       : Text(
+                      //           'Banner Image',
+                      //         ),
+                      // ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.yellow.shade900,
+                      ),
+                      onPressed: () {
+                        // pickImage();
+                      },
+                      child: const Text(
+                        'Upload  Image',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                width: 20,
+              ),
+              const SizedBox(
+                width: 20,
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.yellow.shade900,
+                ),
+                onPressed: () {}, //uploadToFirebase,
+                child: const Text(
+                  'Save',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: 20,
+              )
+            ],
+          ),
+          const Divider(
+            color: Colors.grey,
+          )
         ],
       ),
     );
