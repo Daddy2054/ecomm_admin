2490df540303557d7221ede49fe6014d921fc589
Jean <acalto2@gmail.com>
2023-12-22 21:26:33 +0100

    55. Expanded Widget

--- lib/views/screens/side_bar_screens/vendor_screen.dart
+++ lib/views/screens/side_bar_screens/vendor_screen.dart
@@ -1,26 +1,76 @@
 import 'package:flutter/material.dart';
 
-class VendorScreen extends StatelessWidget {
-  const VendorScreen({super.key});
+class VendorScreen extends StatefulWidget {
   static const String routeName = 'vendorScreen';
 
+  const VendorScreen({super.key});
+
   @override
-  Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          Container(
-            alignment: Alignment.topLeft,
-            padding: const EdgeInsets.all(10),
-            child: const Text(
-              'Manage Vendors',
-              style: TextStyle(
-                fontWeight: FontWeight.w700,
-                fontSize: 36,
+  State<VendorScreen> createState() => _VendorScreenState();
+}
+
+class _VendorScreenState extends State<VendorScreen> {
+  Widget _rowHeader(int flex, String text) {
+    return Expanded(
+        flex: flex,
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey.shade700),
+            color: Colors.yellow.shade900,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              text,
+              style: const TextStyle(
+                color: Colors.white,
               ),
             ),
           ),
-        ],
+        ),);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                alignment: Alignment.topLeft,
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Manage Vendors',
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Row(
+                children: [
+                  _rowHeader(1, 'LOGO'),
+                  _rowHeader(3, 'BUSSINESS NAME'),
+                  _rowHeader(2, 'CITY'),
+                  _rowHeader(2, 'STATE'),
+                  _rowHeader(1, 'ACTION'),
+                  _rowHeader(1, 'VIEW MORE'),
+                ],
+              ),
+              // VendorsList(),
+            ],
+          ),
+        ),
       ),
     );
   }
---
+++ lib/views/widgets/vendors_list.dart
@@ -0,0 +1,137 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+import '../models/vendor_user_models.dart';
+
+class VendorsList extends StatefulWidget {
+  const VendorsList({super.key});
+
+  @override
+  _VendorsListState createState() => _VendorsListState();
+}
+
+class _VendorsListState extends State<VendorsList> {
+  final Stream<QuerySnapshot> _usersStream =
+      FirebaseFirestore.instance.collection('vendors').snapshots();
+
+  Widget vendorData(Widget widget, int? flex) {
+    return Expanded(
+      flex: flex!,
+      child: Padding(
+        padding: const EdgeInsets.all(2.0),
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: widget,
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: _usersStream,
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return const Center(child: Text('Something went wrong'));
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: LinearProgressIndicator());
+        }
+
+        return ListView.builder(
+          shrinkWrap: true,
+          itemCount: snapshot.data!.docs.length,
+          itemBuilder: ((context, index) {
+            VendorUserModel vendor = VendorUserModel.fromJson(
+                snapshot.data!.docs[index].data() as Map<String, dynamic>);
+            return Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                vendorData(
+                    SizedBox(
+                      height: 50,
+                      width: 50,
+                      child: Image.network(
+                        vendor.storeImage.toString(),
+                        width: 50,
+                        height: 50,
+                      ),
+                    ),
+                    1),
+                vendorData(
+                    Text(
+                      vendor.bussinessName.toString(),
+                      style: const TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    3),
+                vendorData(
+                    Text(
+                      vendor.cityValue.toString(),
+                      style: const TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    2),
+                vendorData(
+                    Text(
+                      vendor.stateValue.toString(),
+                      style: const TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    2),
+                vendorData(
+                    vendor.approved == true
+                        ? ElevatedButton(
+                            onPressed: () async {
+                              await FirebaseFirestore.instance
+                                  .collection('vendors')
+                                  .doc(vendor.vendorId)
+                                  .update({
+                                'approved': false,
+                              });
+                            },
+                            child: const Text(
+                              'Reject',
+                              style: TextStyle(
+                                  color: Colors.red,
+                                  fontWeight: FontWeight.bold),
+                            ))
+                        : ElevatedButton(
+                            onPressed: () async {
+                              await FirebaseFirestore.instance
+                                  .collection('vendors')
+                                  .doc(vendor.vendorId)
+                                  .update({
+                                'approved': true,
+                              });
+                            },
+                            child: const Text(
+                              'Approved',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.yellow),
+                            ),
+                          ),
+                    1),
+                vendorData(
+                    ElevatedButton(
+                        onPressed: () {},
+                        child: const Text(
+                          'View More',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        )),
+                    1),
+              ],
+            );
+          }),
+        );
+      },
+    );
+  }
+}
